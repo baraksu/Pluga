f6c40e1eed7be1f64613c31c6edb2ebd8ef5ad4b
ShiloUllman <shilo.ullman@gmail.com>
2023-05-28 22:37:06 +0300

    VERSION  100

--- project misgav01.asm
+++ project misgav01.asm
@@ -31,8 +31,38 @@ mov ax,@data ;data to dataSegment
 mov ds,ax                       
 
 call getMahlaka ;get number of mahlakas
-call getKita ;get number of kitas
-call insertValues ;insert the number of soldiers in each kita
+call getKita ;get number of kitas 
+msg db 13,10,"supported values from -127 to 128",13,10
+     db "enter the number: $"
+; print the message1:
+mov dx, offset msg
+mov ah, 9
+int 21h
+
+xor ax,ax
+xor dx,dx
+xor cx,cx
+call scan_num ; get the number to cx.
+
+mov bx, cx
+
+
+
+
+; wait for any key....
+mov ah, 0
+int 16h
+
+
+ret ; return to operating system.
+; this macro prints a char in al and advances the current cursor position:
+putc    macro   char
+        push    ax
+        mov     al, char
+        mov     ah, 0eh
+        int     10h     
+        pop     ax
+endm
 call NOSIEM ;number of soldiers in each mahlaka
 call maxMahlakaAndKitaSoldiers
 
@@ -92,36 +122,120 @@ proc getKita ;get number of kitas
     ret
 endp getKita
 
-proc insertValues  ;insert the number of soldiers in each kita 
-    lea dx,part_c
-    mov ah,9
-    int 21h
-    mov cx,sizeOfPluga 
-    mov si,0  
-    xor ax,ax
-    mov ah,9
-    lea dx,msg3
-    int 21h
-    lea bx,pluga
-    insertloop:
-        xor ax,ax
-        mov ah,1
-        int 21h
-        cmp al,'0'
-        jb insertloop
-        cmp al,'9'
-        ja insertloop             
-        sub al,30h
+; this procedure gets the multi-digit signed number from the keyboard,
+; and stores the result in cx register:
+scan_num        proc    near
+        push    dx
+        push    ax
+        push    si
         
-        mov [bx+si],al
-        mov ah,9                              
-        lea dx,crlf
-        int 21h
+        mov     cx, 0
+
+        ; reset flag:
+        mov     cs:make_minus, 0
+
+next_digit:
+
+        ; get char from keyboard
+        ; into al:
+        mov     ah, 00h
+        int     16h
+        ; and print it:
+        mov     ah, 0eh
+        int     10h
+        xor ah,ah   ;?reset bios
+        ; check for minus:
+        cmp     al, '-'
+        je      set_minus
+
+        ; check for enter key:
+        cmp     al, 13  ; carriage return?
+        jne     not_cr
+        jmp     stop_input
+not_cr:
+
+
+        cmp     al, 8                   ; 'backspace' pressed?
+        jne     backspace_checked
+        mov     dx, 0                   ; remove last digit by
+        mov     al, cl                  ; division:
+        div     cs:ten                  ; ax = dx:ax / 10 (dx-rem).
+        mov     cl, al
+        putc    ' '                     ; clear position.
+        putc    8                       ; backspace again.
+        jmp     next_digit
+backspace_checked:
+
+
+        ; allow only digits:
+        cmp     al, '0'
+        jae     ok_ae_0
+        jmp     remove_not_digit
+ok_ae_0:        
+        cmp     al, '9'
+        jbe     ok_digit
+remove_not_digit:       
+        putc    8       ; backspace.
+        putc    ' '     ; clear last entered not digit.
+        putc    8       ; backspace again.        
+        jmp     next_digit ; wait for next input.       
+ok_digit:
+
+
+        ; multiply cx by 10 (first time the result is zero)
+        push    ax
+        mov     al,cl
+        mul     cs:ten                  ; dx:ax = ax*10
+        mov     cl, al
+        pop     ax
+
+        ; check if the number is too big
+        ; (result should be 16 bits)
+        cmp     ah, 0
+        jne     too_big
+
+        ; convert from ascii code:
+        sub     al, 30h
+
+        ; add al to cx:
+        mov     ah, 0
+        mov     dl, cl      ; backup, in case the result will be too big.
+        add     cl, al
+        jc      too_big2    ; jump if the number is too big.
+
+        jmp     next_digit
+
+set_minus:
+        mov     cs:make_minus, 1
+        jmp     next_digit
+
+too_big2:
+        mov     cl, dl      ; restore the backuped value before add.
+        mov     dx, 0       ; dx was zero before backup!
+too_big:
+        mov     al, cl
+        div     cs:ten  ; reverse last dx:ax = ax*10, make ax = dx:ax / 10
+        mov     cl, al
+        putc    8       ; backspace.
+        putc    ' '     ; clear last entered digit.
+        putc    8       ; backspace again.        
+        jmp     next_digit ; wait for enter/backspace.
         
-        inc si
-        loop insertloop
-    ret
-endp insertValues
+        
+stop_input:
+        ; check flag:
+        cmp     cs:make_minus, 0
+        je      not_minus
+        neg     cl
+not_minus:
+
+        pop     si
+        pop     ax
+        pop     dx
+        ret
+make_minus      db      ?       ; used as a flag.
+ten             dw      10      ; used as multiplier.
+scan_num        endp
 
 proc NOSIEM ;number of soldiers in each mahlaka
     lea dx,part_d
